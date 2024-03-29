55279507e8f55de16fc9fc65cfbf6bf21ac12bf7
taichihub <taichikuniv21@gmail.com>
2024-03-29 14:48:54 +0900

    無駄なインデント削除

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -44,7 +44,7 @@ module ReversiMethods
     pos = Position.new(cell_ref)
     raise '無効なポジションです' if pos.invalid?
     raise 'すでに石が置かれています' unless pos.stone_color(board) == BLANK_CELL
-    
+
     # コピーした盤面にて石の配置を試みて、成功すれば反映する
     copied_board = Marshal.load(Marshal.dump(board))
     copied_board[pos.row][pos.col] = stone_color
